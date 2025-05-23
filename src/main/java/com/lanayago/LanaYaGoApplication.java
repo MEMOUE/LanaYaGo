38c5e9bcce313b0f8a78dd3595e1931f632a339d
MEMOUE <konememoue1998@gmail.com>
2025-05-23 18:55:05 +0000

    feat: projet init

---
+++ src/main/java/com/lanayago/LanaYaGoApplication.java
@@ -0,0 +1,13 @@
+package com.lanayago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanaYaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanaYaGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanayago/LanaYaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanayago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanaYaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
